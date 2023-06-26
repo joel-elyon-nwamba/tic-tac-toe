8b52f8726670e56f0f953ec496a89e42ccdb3db0
Joel Nwamba <joel.elyon.nwamba@gmail.com>
2023-06-26 10:15:35 -0400

    still working on fixing on getting the results once player wins

---
+++ js/script.js
@@ -0,0 +1,51 @@
+const Gameboard = (() => {
+  const gameboard = ["", "", "", "", "", "", "", "", ""];
+  const playerInformation = (name, marker) => {
+      return {name, marker}
+  }
+  const playerOne = playerInformation("player-one", "X");
+  const playerTwo = playerInformation("player-two", "O");
+  let currentPlayer = playerOne.marker;
+  let playerTurn = playerTwo.marker;
+  /*
+  Set the current player up
+  and set the following the player number 2's turn
+   */
+
+  /* 
+  grab the class cell to have access to the board
+  */
+
+  /*
+  Here we're gonna create winning conditions for the game
+   */
+  const winningConditions = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+  ];
+  const checkScore = () => {
+      /**
+       * iterate through the arraye
+       */
+     let roundWinner = false;
+     for(let i = 0; i < winningConditions.length; i++) {
+      const winCombo = winningConditions[i];
+      let a = gameboard[winCombo[0]];
+      let b = gameboard[winCombo[1]];
+      let c = gameboard[winCombo[2]];
+      if(a === "" || b === "" || c ===  "") {
+          continue;
+      } else if(a === b && b === c) {
+          roundWinner = true;
+          break;
+      }
+     }
+  }
+
+})();
