e3b523809beaa264ea08f6fcdb4820be1db60d3b
Joel Nwamba <joel.elyon.nwamba@gmail.com>
2023-06-27 09:18:36 -0400

    selected classes for cells, retart game, new game to be interactive, now need to initialize game for next session

--- js/script.js
+++ js/script.js
@@ -11,14 +11,23 @@ const Gameboard = (() => {
   Set the current player up
   and set the following the player number 2's turn
    */
+  
 
   /* 
   grab the class cell to have access to the board
   */
-
+  const cells = document.querySelectorAll(".cell");
+  /*
+  Select the restart button
+  */
+ const restartBtn = document.querySelector(".restart-btn");
+  /*
+  button to start new game
+   */
+  const newGame = document.querySelector(".new-btn");
   /*
   Here we're gonna create winning conditions for the game
-   */
+  */
   const winningConditions = [
       [0, 1, 2],
       [3, 4, 5],
@@ -29,7 +38,7 @@ const Gameboard = (() => {
       [0, 4, 8],
       [2, 4, 6]
   ];
-  const checkScore = () => {
+  const checkWinner = () => {
       /**
        * iterate through the arraye
        */
